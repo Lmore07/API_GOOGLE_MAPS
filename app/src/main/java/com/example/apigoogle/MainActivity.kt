372a5bacee3124a6dcc550522af2cc7bccaece09
Lmore07 <lmoreirat@uteq.edu.ec>
2022-08-19 16:04:41 -0500

    asd

--- app/src/main/java/com/example/apigoogle/MainActivity.kt
+++ app/src/main/java/com/example/apigoogle/MainActivity.kt
@@ -13,7 +13,6 @@ import com.google.android.gms.maps.model.PolylineOptions
 class MainActivity : FragmentActivity(), OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
     lateinit var mMap:GoogleMap
-    lateinit var lista:ArrayList<LatLng>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,28 +27,31 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback, GoogleMap.OnMapClic
         mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         mMap.getUiSettings().setZoomControlsEnabled(true)
         var camUpd1 =
-            CameraUpdateFactory.newLatLngZoom(LatLng(-1.080428, -79.501368), 19F);
+            CameraUpdateFactory.newLatLngZoom(LatLng(-1.080428, -79.501368), 19F)
         mMap.moveCamera(camUpd1)
         mMap.setOnMapClickListener(this);
+        lista=PolylineOptions()
     }
+
+    lateinit var lista:PolylineOptions
+    var puntos=0
 
     override fun onMapClick(point: LatLng) {
         var punto = LatLng(point.latitude,point.longitude);
-        lista.add(point)
+        if(puntos<5){
+            lista.add(point)
+            puntos++;
+        }
+        if(puntos==5){
+            lista.width(8F);
+            lista.color(Color.RED);
+            mMap.addPolyline(lista);
+            puntos=0;
+            lista=PolylineOptions()
+        }
+
         mMap.addMarker(
             MarkerOptions().position(punto));
-
-        var lineas = PolylineOptions()
-            .add(LatLng(45.0, -12.0))
-            .add(LatLng(45.0, 5.0))
-            .add(LatLng(34.5, 5.0))
-            .add(LatLng(34.5, -12.0))
-            .add(LatLng(45.0, -12.0));
-        lineas.width(8F);
-        lineas.color(Color.RED);
-        mMap.addPolyline(lineas);
-
-
         Toast.makeText(
         this,"Click\n" +"Lat: " + point.latitude + "\n" +"Lng: " + point.longitude + "\n",
         Toast.LENGTH_SHORT).show();
